62bdc3c392272e74dcc3f38522744c60ef9b3916
KiaNicole <43224069+KiaNicole@users.noreply.github.com>
2018-10-22 17:15:01 -0400

    Files for SMS (working version)
    This code works.  Don't use the other one.

--- sendsms.php
+++ sendsms.php
@@ -10,7 +10,6 @@ require __DIR__ . '/twilio-php-master/Twilio/autoload.php';
 use Twilio\Rest\Client;
 
 // Your Account SID, Auth Token, and Twilio phone number from twilio.com/console
-$smsMessage = $_POST["yourMessage"];
 $twilioPhone = '+13025817175';
 $sid = 'AC89a94ed31367669484d648d1157dcadd';
 $token = 'c96131fb4b572ca3844d9de828e0832d';
@@ -22,10 +21,10 @@ $client->messages->create(
     '+12408328669',
     array(
         // A Twilio phone number you purchased at twilio.com/console
-        'from' => '$twilioPhone',
+        'from' => $twilioPhone,
         // the body of the text message you'd like to send
 		//"Hey Jenny! Good luck on the bar exam!"
-        'body' => '$smsMessage'
+        'body' => $_POST["yourMessage"]
     )
 );
 
